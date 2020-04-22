9ca276a2c189e46f2e7bd11db9ca388c3f07f45f
kbartelt <39836333+kbartelt@users.noreply.github.com>
2020-04-22 13:43:53 -0600

    remove hard coded paths

--- scripts/create_files.py
+++ scripts/create_files.py
@@ -15,9 +15,9 @@ import os
 import argparse
 
 
-# define folder with template shapefiles and output folder
-template_folder(r"C:\Users\karen\Box\0_ET_AL\NonProject\etal_Drone\2019\Inundation_sites\Utah\Franklin_basin\06052019\GIS")
-out_path = r"C:\Users\karen\Box\0_ET_AL\NonProject\etal_Drone\2019\Inundation_sites"
+# define folder with a template file in the desired coordinate system and output folder
+srs_template = 
+out_path = 
 
 # Set local variables
 has_m = "DISABLED"
@@ -26,27 +26,21 @@ has_z = "DISABLED"
 
 
 # Use Describe to get a SpatialReference object
-spatial_reference = arcpy.Describe(inundation_template).spatialReference
+spatial_reference = arcpy.Describe(srs_template).spatialReference
 
 # Execute CreateFeatureclass
 
 # inundation
-arcpy.CreateFeatureclass_management(out_path, "inundation.shp", "POLYGON", inundation_template, has_m, has_z, spatial_reference)
+arcpy.CreateFeatureclass_management(out_path, "inundation.shp", "POLYGON", "",has_m, has_z, spatial_reference)
 
 # dam crests
-arcpy.CreateFeatureclass_management(out_path, "dam_crests.shp", "POLYLINE", dam_crests_template, has_m, has_z, spatial_reference)
+arcpy.CreateFeatureclass_management(out_path, "dam_crests.shp", "POLYLINE", "", has_m, has_z, spatial_reference)
 
 # valley bottom
-arcpy.CreateFeatureclass_management(out_path, "valley_bottom.shp", "POLYGON", valleybottom_template, has_m, has_z, spatial_reference)
+arcpy.CreateFeatureclass_management(out_path, "valley_bottom.shp", "POLYGON", "", has_m, has_z, spatial_reference)
 
 # valley bottom centerline
-arcpy.CreateFeatureclass_management(out_path, "vb_centerline.shp", "POLYLINE", vbcenterline_template, has_m, has_z, spatial_reference)
+arcpy.CreateFeatureclass_management(out_path, "vb_centerline.shp", "POLYLINE", "", has_m, has_z, spatial_reference)
 
 # thalwegs
-arcpy.CreateFeatureclass_management(out_path, "thalwegs.shp", "POLYLINE", thalwegs_template, has_m, has_z, spatial_reference)
-
-# pre beaver inundation
-arcpy.CreateFeatureclass_management(out_path, "inundation_pre.shp", "POLYGON", inundation_template, has_m, has_z, spatial_reference)
-
-# valley bottom
-arcpy.CreateFeatureclass_management(out_path, "active_channel.shp", "POLYGON", valleybottom_template, has_m, has_z, spatial_reference)
+arcpy.CreateFeatureclass_management(out_path, "thalwegs.shp", "POLYLINE", "", has_m, has_z, spatial_reference)
