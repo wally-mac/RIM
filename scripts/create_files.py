d3654f043274f540c2f45102098714aff63c17ee
Karen Bartelt <karen.m.bartelt@gmail.com>
2020-04-30 10:53:45 -0600

    make create_files a function

--- scripts/create_files.py
+++ scripts/create_files.py
@@ -16,35 +16,45 @@ import argparse
 
 
 # provide a shapefile with the desired output coordinate system and define the output folder
-#srs_template = 
-#out_path = 
+srs_template = 
+out_path = 
 
-# Set local variables
-has_m = "DISABLED"
-has_z = "DISABLED"
+#function for create files
+def create_files(srs_template, out_path)
 
+    # Set local variables
+    has_m = "DISABLED"
+    has_z = "DISABLED"
 
+    # Use Describe to get a SpatialReference object
+    spatial_reference = arcpy.Describe(srs_template).spatialReference
 
-# Use Describe to get a SpatialReference object
-spatial_reference = arcpy.Describe(srs_template).spatialReference
+    # Execute CreateFeatureclass
 
-# Execute CreateFeatureclass
+    # inundation
+    arcpy.CreateFeatureclass_management(out_path, "inundation.shp", "POLYGON", "",has_m, has_z, spatial_reference)
+    #add field for inundation type
+    arcpy.management.AddFields(os.path.join(out_path, 'inundation.shp'), ['type', 'TEXT'])
 
-# inundation
-arcpy.CreateFeatureclass_management(out_path, "inundation.shp", "POLYGON", "",has_m, has_z, spatial_reference)
-#add field for inundation type
-arcpy.management.AddFields(os.path.join(out_path, 'inundation.shp'), ['type', 'TEXT'])
+    # dam crests
+    arcpy.CreateFeatureclass_management(out_path, "dam_crests.shp", "POLYLINE", "", has_m, has_z, spatial_reference)
+    #add fields for dam state and crest type
+    arcpy.management.AddFields(os.path.join(out_path, 'inundation.shp'), [['dam_state', 'TEXT'], ['crest_type', 'TEXT']])
 
-# dam crests
-arcpy.CreateFeatureclass_management(out_path, "dam_crests.shp", "POLYLINE", "", has_m, has_z, spatial_reference)
-#add fields for dam state and crest type
-arcpy.management.AddFields(os.path.join(out_path, 'inundation.shp'), [['dam_state', 'TEXT'], ['crest_type', 'TEXT"]])
+    # valley bottom
+    arcpy.CreateFeatureclass_management(out_path, "valley_bottom.shp", "POLYGON", "", has_m, has_z, spatial_reference)
 
-# valley bottom
-arcpy.CreateFeatureclass_management(out_path, "valley_bottom.shp", "POLYGON", "", has_m, has_z, spatial_reference)
+    # valley bottom centerline
+    arcpy.CreateFeatureclass_management(out_path, "vb_centerline.shp", "POLYLINE", "", has_m, has_z, spatial_reference)
 
-# valley bottom centerline
-arcpy.CreateFeatureclass_management(out_path, "vb_centerline.shp", "POLYLINE", "", has_m, has_z, spatial_reference)
+    # thalwegs
+    arcpy.CreateFeatureclass_management(out_path, "thalwegs.shp", "POLYLINE", "", has_m, has_z, spatial_reference)
 
-# thalwegs
-arcpy.CreateFeatureclass_management(out_path, "thalwegs.shp", "POLYLINE", "", has_m, has_z, spatial_reference)
+def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('srs_template', help='path to a shapefile with desired output coordinate system', type=str)
+    parser.add_argument('out_path', help='path to output folder', type=str)
+    args = parser.parse_args()
+
+    create_files(args.srs_template, args.out_path)
--- scripts/template_folder.py
+++ scripts/template_folder.py
@@ -3,30 +3,30 @@ import argparse
 
 # function to define folder with template shapefiles
 
-def template_folder(template_path):
+def folder(output_path):
     
-    inundation_template = os.path.join(template_path, 'inundation.shp')
+    inundation_template = os.path.join(output_path, 'inundation.shp')
     print(inundation_template)
     
-    dam_crests_template = os.path.join(template_path, 'dam_crests.shp')
+    dam_crests_template = os.path.join(output_path, 'dam_crests.shp')
     print(dam_crests_template)
 
-    valleybottom_template = os.path.join(template_path, 'valley_bottom.shp')
+    valleybottom_template = os.path.join(output_path, 'valley_bottom.shp')
     print(valleybottom_template)
 
-    vbcenterline_template = os.path.join(template_path, 'vb_centerline.shp')
+    vbcenterline_template = os.path.join(output_path, 'vb_centerline.shp')
     print(vbcenterline_template)
 
-    thalwegs_template = os.path.join(template_path, 'thalwegs.shp')
+    thalwegs_template = os.path.join(output_path, 'thalwegs.shp')
     print(thalwegs_template)
 
 def main():
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('template_path', help='folder path with template shapefiles', type=str)
+    parser.add_argument('output_path', help='folder path with template shapefiles', type=str)
     args = parser.parse_args()
     
-    template_folder(args.template_path)
+    template_folder(args.output_path)
     
 if __name__ == "__main__":
    main()     
