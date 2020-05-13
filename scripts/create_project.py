d540cd7acba98c346f2c5b7908d6b222fba29046
Karen Bartelt <karen.m.bartelt@gmail.com>
2020-05-13 13:33:19 -0600

    also create empty shapefiles for first DCE

--- scripts/create_project.py
+++ scripts/create_project.py
@@ -1,6 +1,11 @@
+# Description: Create project folders and empty shapefiles for the first Data capture event
+
 import os
 import arcpy
 import sys
+
+# path to a shapefile with the desired output coordinate system
+#srs_template = r"C:\Users\karen\Box\0_ET_AL\NonProject\etal_Drone\2019\Inundation_sites\Utah\Mill_Creek\08042019\GIS\dam_crests.shp"
 
 # Make folder function 
 # copied from pyBRAT SupportingFunctions.py
@@ -16,7 +21,7 @@ def make_folder(path_to_location, new_folder_name):
         os.mkdir(newFolder)
     return newFolder
 
-def make_project(project_path):
+def make_project(project_path, srs_template):
     """
     Creates project folders
     :param project_path: where we want project to be located
@@ -46,11 +51,36 @@ def make_project(project_path):
     make_folder(context_folder, "VBET_01")
 
     # mapping folder
-    # DCE and RS folders are created when a DCE is made
-    make_folder(project_path, "02_Mapping")
-
+    # subsequent DCE and RS folders are created when a new DCE is made using new dce script
+    mapping_folder = make_folder(project_path, "02_Mapping")
+    DCE01_folder =  make_folder(mapping_folder, "DCE_01")
+    # make empty shapefiles for first DCE
+    # Use Describe to get a SpatialReference object
+    spatial_reference = arcpy.Describe(srs_template).spatialReference
+    # inundation
+    arcpy.CreateFeatureclass_management(DCE01_folder, "inundation.shp", "POLYGON", "", "DISABLED", "DISABLED", spatial_reference)
+    #add field for inundation type
+    arcpy.AddField_management(os.path.join(DCE01_folder, 'inundation.shp'), 'type', "TEXT")
+    # dam crests
+    arcpy.CreateFeatureclass_management(DCE01_folder, "dam_crests.shp", "POLYLINE", "", "DISABLED", "DISABLED", spatial_reference)
+    #add fields for dam state and crest type
+    arcpy.AddField_management(os.path.join(DCE01_folder, 'dam_crests.shp'), 'dam_state', "TEXT")
+    arcpy.AddField_management(os.path.join(DCE01_folder, 'dam_crests.shp'), 'crest_type', "TEXT")
+    # thalwegs
+    arcpy.CreateFeatureclass_management(DCE01_folder, "thalwegs.shp", "POLYLINE", "", "DISABLED", "DISABLED", spatial_reference)
+    #add fields for thalweg type
+    arcpy.AddField_management(os.path.join(DCE01_folder, 'thalwegs.shp'), 'type', "TEXT")
+    # make first RS folder
+    RS01_folder = make_folder(mapping_folder, "RS_01")
+    # create empty shapefiles for valley bottom and valley bottom centerline
+    # valley bottom
+    arcpy.CreateFeatureclass_management(RS01_folder, "valley_bottom.shp", "POLYGON", "", "DISABLED", "DISABLED", spatial_reference)
+    # valley bottom centerline
+    arcpy.CreateFeatureclass_management(RS01_folder, "vb_centerline.shp", "POLYLINE", "", "DISABLED", "DISABLED", spatial_reference)
+    
     # analysis folder
     analysis_folder = make_folder(project_path, "03_Analysis")
+    make_folder(analysis_folder, "DCE_01")
     make_folder(analysis_folder, "CDs")
     make_folder(analysis_folder, "Summary")
 
@@ -63,8 +93,11 @@ def main():
 
     make_project(project_path)
 
-#project_path = r"C:\Users\karen\Box\0_ET_AL\NonProject\etal_Drone\2019\Inundation_sites\Utah\rock_creek_b\rock_creek_b"
-make_project(project_path)
+project_path = r"C:\Users\karen\Box\0_ET_AL\NonProject\etal_Drone\2019\Inundation_sites\Utah\rock_creek_b\rock_creek_b"
+# path to a shapefile with the desired output coordinate system
+srs_template = r"C:\Users\karen\Box\0_ET_AL\NonProject\etal_Drone\2019\Inundation_sites\Utah\Mill_Creek\08042019\GIS\dam_crests.shp"
+
+make_project(project_path, srs_template)
 
 
 
