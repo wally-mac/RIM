846d240ffaaad4b70138f72fecc1502f6f3e92a2
Karen Bartelt <A02295870@aggies.usu.edu>
2020-08-16 13:12:03 -0600

    add additional image to project folder

--- create_DCE.py
+++ create_DCE.py
@@ -25,7 +25,9 @@ from lib.loghelper import Logger
 # name of desired new DCE folder
 DCE_fold = 'DCE_02'
 
-#function for create files
+# function for create files
+
+
 def new_DCE(srs_template, project_path, DCE_fold):
 
     log = Logger('new_DCE')
@@ -39,14 +41,30 @@ def new_DCE(srs_template, project_path, DCE_fold):
     spatial_reference = arcpy.Describe(srs_template).spatialReference
 
     log.info('checking if project folders exist')
-    #check if Inputs, Mapping, and Analysis folders exist, if not create them
+    # check if Inputs, Mapping, and Analysis folders exist, if not create them
     folder_list = ['01_Inputs', '02_Mapping', '03_Analysis']
     for folder in folder_list:
         if not os.path.exists(os.path.join(project_path, folder)):
             os.makedirs(os.path.join(project_path, folder))
 
-    log.info('Inputs, Mapping, Analysis folders exist')    
-    
+    log.info('Inputs, Mapping, Analysis folders exist')
+
+    # set pathway to imagery folder
+    image_folder = os.path.join(project_path, '01_Imagery')
+
+    # create new AP folder
+    if not os.path.exists(os.path.join(image_folder, AP_fold)):
+        AP_path = os.makedirs(os.path.join(image_folder, AP_fold))
+
+    log.info('copying image to project folder...')
+
+    def add_image(image_path, AP_folder):
+        # put input imagery in folder
+        if not os.path.exists(os.path.join(AP_folder, 'imagery.tif')):
+            arcpy.CopyRaster_management(image_path, os.path.join(AP_folder, 'imagery.tif'))
+            else print('existing image already exists in this AP folder')
+    add_image(image_path, AP_path)
+
     # set pathway to mapping folder
     map_path = os.path.join(project_path, '02_Mapping')
 
@@ -54,23 +72,23 @@ def new_DCE(srs_template, project_path, DCE_fold):
     if not os.path.exists(os.path.join(map_path, 'RS_01')):
         os.makedirs(os.path.join(map_path, 'RS_01'))
 
-    #create new DCE folder
+    # create new DCE folder
     if not os.path.exists(os.path.join(map_path, DCE_fold)):
-        os.makedirs(os.path.join(map_path, DCE_fold)) 
+        os.makedirs(os.path.join(map_path, DCE_fold))
 
-    #create a folder in Analysis for this DCE
+    # create a folder in Analysis for this DCE
     analysis_path = os.path.join(project_path, '03_Analysis')
     if not os.path.exists(os.path.join(analysis_path, DCE_fold)):
         os.makedirs(os.path.join(analysis_path, DCE_fold))
 
     # inundation
-    arcpy.CreateFeatureclass_management(os.path.join(map_path, DCE_fold), "inundation.shp", "POLYGON", "",has_m, has_z, spatial_reference)
-    #add field for inundation type
+    arcpy.CreateFeatureclass_management(os.path.join(map_path, DCE_fold), "inundation.shp", "POLYGON", "", has_m, has_z, spatial_reference)
+    # add field for inundation type
     arcpy.AddField_management(os.path.join(map_path, DCE_fold, 'inundation.shp'), 'type', "TEXT")
 
     # dam crests
     arcpy.CreateFeatureclass_management(os.path.join(map_path, DCE_fold), "dam_crests.shp", "POLYLINE", "", has_m, has_z, spatial_reference)
-    #add fields for dam state and crest type
+    # add fields for dam state and crest type
     arcpy.AddField_management(os.path.join(map_path, DCE_fold, 'dam_crests.shp'), 'dam_state', "TEXT")
     arcpy.AddField_management(os.path.join(map_path, DCE_fold, 'dam_crests.shp'), 'crest_type', "TEXT")
     arcpy.AddField_management(os.path.join(map_path, DCE_fold, 'dam_crests.shp'), 'dam_id', "DOUBLE")
@@ -78,8 +96,9 @@ def new_DCE(srs_template, project_path, DCE_fold):
     # thalwegs
     arcpy.CreateFeatureclass_management(os.path.join(map_path, DCE_fold), "thalwegs.shp", "POLYLINE", "", has_m, has_z, spatial_reference)
 
+
 def main():
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument('srs_template', help='path to a shapefile with desired output coordinate system', type=str)
     parser.add_argument('project_path', help='path to output folder', type=str)
