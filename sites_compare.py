423e2379b828149624c2e40e2e8b9066bd81dbd5
Karen Bartelt <A02295870@aggies.usu.edu>
2020-08-20 16:12:13 -0600

    script to combine all site csvs to a master csv

---
+++ sites_compare.py
@@ -0,0 +1,14 @@
+import os
+import glob
+import pandas as pd
+
+
+path = r"C:\Users\A02295870\Box\Thesis_sites"
+
+all_files = glob.glob(path + "/**/**/**/**/**/valley_bottom_metrics.csv")
+
+#all_files = [r"C:\Users\A02295870\Box\0_ET_AL\NonProject\etal_Drone\2019\Inundation_sites\Utah\Mill_Creek\codetest_0817\03_Analysis\DCE_01\01_Metrics\valley_bottom_metrics.csv", r"C:\Users\A02295870\Box\0_ET_AL\NonProject\etal_Drone\2019\Inundation_sites\Utah\Mill_Creek\codetest_0817\03_Analysis\DCE_02\01_Metrics\valley_bottom_metrics.csv", r"C:\Users\A02295870\Box\Thesis_sites\metrics.csv"]
+
+metrics2 = pd.concat([pd.read_csv(f) for f in all_files], verify_integrity=True)
+
+metrics2.to_csv(os.path.join(path, "metrics2.csv"))
