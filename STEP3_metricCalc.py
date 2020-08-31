65c7631057f5d1d6555a1075ed546cde67a5c0d2
Karen Bartelt <A02295870@aggies.usu.edu>
2020-08-31 13:46:06 -0600

    add function call

--- STEP3_metricCalc.py
+++ STEP3_metricCalc.py
@@ -1,5 +1,6 @@
 # Import system modules
 
+from calculate_metrics import calculate_metrics
 import arcpy
 from arcpy import env
 import os
@@ -14,7 +15,7 @@ arcpy.CheckOutExtension('Spatial')
 # Inputs
 
 # project path
-project_path = r"C:\Users\karen\Box\0_ET_AL\NonProject\etal_Drone\2019\Inundation_sites\Utah\Mill_Creek\codetest_0622"
+project_path = r"C:\Users\A02295870\Box\Thesis_sites\16010203\little_bear_low"
 # name of the folder of the desired RS Context shapefiles (the folder with the Valley Bottom polygon)
 RS_folder_name = "RS_01"
 # path to DEM to be used to calculate slopes
@@ -22,22 +23,22 @@ DEM = os.path.join(project_path, '01_Inputs', '02_Topo', 'DEM_01', 'DEM.tif')
 
 # DCE Parameters
 # site parameters
-site_name = "mill_creek"
-
+site_name = "little_bear_low"
 # name of the folder of the desired DCEs for the analysis
 DCE1_name = "DCE_01"
-DCE2_name = "DCE_02"
+DCE2_name = 'DCE_02'
 # DCE 1 Parameters
-DCE1_date = '20190804'
+DCE1_date = "20190830"
 DCE1_flow_stage = 'low'
-DCE1_active = 'yes'
-DCE1_maintained = 'yes'
+DCE1_active = 'no'
+DCE1_maintained = 'no'
 DCE1_res = '.02'
 # DCE 2 Parameters
-DCE2_date = 'pre beaver'
+DCE2_date = 'estimated_pre'
 DCE2_flow_stage = 'low'
 DCE2_active = 'NA'
 DCE2_maintained = 'NA'
-DCE2_res = '.2'
+DCE2_res = '.46'
 
+calculate_metrics(project_path, RS_folder_name, DEM, site_name, DCE1_name, DCE1_date, DCE1_flow_stage, DCE1_active, DCE1_maintained, DCE2_name, DCE2_date, DCE2_flow_stage, DCE2_active, DCE2_maintained, DCE1_res, DCE2_res)
 ################################
