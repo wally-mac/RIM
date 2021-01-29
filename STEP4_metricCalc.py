d0ad6181c86fb08833eeb41ed4029e5dd70890a4
Karen Bartelt <A02295870@aggies.usu.edu>
2021-01-29 08:09:28 -0700

    Update STEP4_metricCalc.py

--- STEP4_metricCalc.py
+++ STEP4_metricCalc.py
@@ -15,15 +15,19 @@ arcpy.CheckOutExtension('Spatial')
 # Inputs
 
 # project path
-project_path = r"C:\Users\A02295870\Box\Thesis_sites\16010203\RH_fork_a_2"
+project_path = r"C:\Users\A02295870\Documents\RH_Fork_mid_T1"
 # name of the folder of the desired RS Context shapefiles (the folder with the Valley Bottom polygon)
 RS_folder_name = "RS_01"
 # path to DEM to be used to calculate slopes
 DEM = os.path.join(project_path, '01_Inputs', '02_Topo', 'DEM_01', 'DEM.tif')
 
+# metadata
+mapper = 'Karen Bartelt'
+
 # DCE Parameters
 # site parameters
-site_name = "RH_fork_a_2"
+project_name = "RH Fork Mid Test 1"  # readable version of site name
+site_name = "RH_fork_mid_T1"  # shorthand version
 huc8 = '16010203'
 setting = "classic"  # classic, steep, or floodplain
 
@@ -32,18 +36,22 @@ DCE1_name = "DCE_01"
 DCE2_name = 'DCE_02'
 
 # DCE 1 Parameters
-DCE1_date = "20190502
-DCE1_flow_stage = 'high'
+DCE1_date = "201108"
+DCE1_image_source = 'drone'
+DCE1_date_name = "August 2019"
+DCE1_flow_stage = 'moderate'
 DCE1_active = 'Yes'
 DCE1_maintained = 'No'
 DCE1_res = '.02'
 # DCE 2 Parameters
-DCE2_date = '20191103'
-DCE2_flow_stage = 'low'
+DCE2_date = '201108'
+DCE2_image_source = 'google earth'
+DCE2_date_name = "Undammed"
+DCE2_flow_stage = 'moderate'
 DCE2_active = 'Yes'
 DCE2_maintained = 'Yes'
-DCE2_res = '.02'
+DCE2_res = '.46'
 
 
-calculate_metrics(project_path, RS_folder_name, DEM, site_name, DCE1_name, DCE1_date, DCE1_flow_stage, DCE1_active, DCE1_maintained, DCE2_name, DCE2_date, DCE2_flow_stage, DCE2_active, DCE2_maintained, DCE1_res, DCE2_res, setting, huc8)
+calculate_metrics(project_path, RS_folder_name, DEM, mapper, project_name, site_name, DCE1_name, DCE1_date, DCE1_image_source, DCE2_image_source, DCE1_date_name, DCE2_date_name, DCE1_flow_stage, DCE1_active, DCE1_maintained, DCE2_name, DCE2_date, DCE2_flow_stage, DCE2_active, DCE2_maintained, DCE1_res, DCE2_res, setting, huc8)
 ################################
